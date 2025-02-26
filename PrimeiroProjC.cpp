64ce7b3922bec870f251eb60382fa609fc18c73b
Thales <136759198+ManoPedra@users.noreply.github.com>
2025-02-25 22:50:44 -0300

    Add files via upload

---
+++ PrimeiroProjC.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hello World!\n";
+  cout << "How is the WeAtHeR? \n";
+  cout << "heres a number for you:";
+  cout << 931 << "\n";
+  cout << "if we subtract it by 291 it will be: ";
+  cout << 931-291 << "\n";
+  cout << "now, if the number is doubled it shall be ->  ";
+  cout << 931 * 2;
+  return 0;
+}
