b3463d6b2c7cad6adcf80f03daab353f4603fe70
Thales <136759198+ManoPedra@users.noreply.github.com>
2025-03-03 22:37:07 -0300

    2 conversas pra testar variaveis e só

---
+++ Converse.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+//this is a comment
+/* this is a cooler comment*/
+//below are variables
+int main (){
+    int Num = 90;
+    double Price = 1.90;
+    double SecPrice = 4.20;
+    string Text = "The PCman";
+
+    double TJDprice = Price + SecPrice;
+
+        cout << "i am "<< Text <<"\n";
+        cout << "the number that i got for you gentleman today is "<< Num << "\n";
+        cout << "i today have made me a purchase of a very fine WeirdAppter™ for mere "<< SecPrice << "$!\n" ;
+        cout << "it has made most pleasant day for me. \n" ;
+        cout << "i too have bought a most delightfull beverage for "<< Price <<"$. \n" ;
+        cout << "its name sounded very weird for my person to pronunciate, since i do not own a talking organ that is, \n but it was indeed very delightfull. \n";
+        cout << "i have made quite the purchase in this fine day, a jawdropping "<< TJDprice <<" $ Currency! \n" ;
+        cout << "but alas, the purchase has made me very pleased that is true. \n";
+
+ return 0;
+}
+
+
---
+++ How is the Weather.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+    cout << "hello world \n";
+    cout << "how is the Weather? \n";
+    cout << "are you polite and good? [Y/N]";
+
+    return 0;
+    
+}
