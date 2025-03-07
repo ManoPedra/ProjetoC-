acd6f7f42722e993b6f73e3d66057d16b7923a34
Thales <136759198+ManoPedra@users.noreply.github.com>
2025-03-07 17:10:16 -0300

    uma calculadora de soma simples

---
+++ Calculadora_de_soma_simples.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+ float PriNum;
+ float SegNum;
+ float Res;
+ cout << "digite um numero: ";
+ cin >> PriNum;
+ cout <<"\n"<<"digite outro numero: ";
+ cin >> SegNum;
+
+ Res = PriNum + SegNum;
+ cout<< "\n"<<"o resultado dos dois e igual a:"<< Res <<".";
+ return 0;
+}
