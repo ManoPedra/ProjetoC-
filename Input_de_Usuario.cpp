8517c436a8f964ef248235ca3686453207270323
Thales <136759198+ManoPedra@users.noreply.github.com>
2025-03-07 17:09:45 -0300

    Conversa com Inputs dessa vez

---
+++ Input_de_Usuario.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+
+ string Name;
+ string answer1;
+
+    cout<< "hello! \n";
+    cout<< "who am i talking to?  \n";
+    cin >> Name;
+    cout << "ah yes,"<< Name <<" ! i remember talking to you before about a purchase, yes? \n";
+    cin >> answer1;
+    cout << "\n i see...";
+
+ return 0;
+}
