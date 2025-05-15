cee84a2ae3049baf8871729253f47e5492d42e76
Thales <136759198+ManoPedra@users.noreply.github.com>
2025-05-15 19:56:16 -0300

    Exercitando operadores
    é um executavel que pede para o usuario digitar 2 valores para utilizar em operações simples de aritmética

---
+++ Exercicio-Operadores-15_05.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+//Operadores de Aritmética
+int main(){
+int num1, num2 ;
+    cout << "hello world!";
+    cout << "\ninput a value to assign to num1: ";
+    cin >> num1;
+    cout << "input another value to assign to num2: ";
+    cin >> num2;
+    cout << "\nthe variable num1 has stored: " << num1 <<" and num2 has stored: " << num2;
+    cout << "\nthe sum of num1 and num2 equals to: " << num1 + num2 ;
+    cout << "\nthe difference of the variables num1 and num2 are: " << num1 - num2;
+    cout << "\nthe product of the assigned computer vectors num1 and num2 are: " << num1 * num2;
+    cout << "\nthe quotient of the variables num1 and num2 are: " << num1/num2;
+    cout << "\nthe division remainder of the variables num1 and num2 are: " << num1 % num2;
+    cout << "\nthe variable num1 will have its value increased by 1: " << ++num1;
+    cout << "\nthe variable num2 will have its value decreased by 1: " << --num2;
+
+return 0;
+}
